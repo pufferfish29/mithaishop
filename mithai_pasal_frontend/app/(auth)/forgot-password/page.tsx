124024521e871cad0e0017fb6c4b44c01399197f
bisekhkarki <karkidai79@gmail.com>
2025-10-23 06:14:34 +0545

    Product add and update ui completed

--- mithai_pasal_frontend/app/(auth)/forgot-password/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChangePassword from './_components/ChangePassword';
-
-const ForgotPassword = () => {
-  return <ChangePassword />;
-};
-
-export default ForgotPassword;
