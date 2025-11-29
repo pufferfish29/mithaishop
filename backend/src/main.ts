d50a4fbc0d41e27c2d63c178eaa26d55b087cd45
rudy <fluffyrudy@proton.me>
2025-11-29 16:14:46 +0545

    allow all origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:3001",
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
@@ -19,7 +19,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       forbidUnknownValues: true,
       transform: true,
-    })
+    }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
